67ddce5d008c57c50af3999c446b280baa3e5b50
!git for-each-ref --format='%(refname:short)' `git symbolic-ref HEAD` <ben.dichter@gmail.com>
2020-01-02 13:58:52 -0800

    clean up specpath

--- src/pynwb/ndx_point_cloud_table/point_cloud_table.py
+++ src/pynwb/ndx_point_cloud_table/point_cloud_table.py
@@ -6,22 +6,7 @@ from hdmf.utils import docval, getargs, popargs, call_docval_func, get_docval
 
 name = 'ndx-point-cloud-table'
 # Set path of the namespace.yaml file to the expected install location
-ndx_point_cloud_table_specpath = os.path.join(
-    os.path.dirname(__file__),
-    'spec',
-    name + '.namespace.yaml'
-)
-
-# If the extension has not been installed yet but we are running directly from
-# the git repo
-if not os.path.exists(ndx_point_cloud_table_specpath):
-    ndx_point_cloud_table_specpath = os.path.abspath(os.path.join(
-        os.path.dirname(__file__),
-        '..', '..', '..',
-        'spec',
-        name + '.namespace.yaml'
-    ))
-
+ndx_point_cloud_table_specpath = os.path.join(os.path.dirname(__file__), 'spec', name + '.namespace.yaml')
 
 load_namespaces(ndx_point_cloud_table_specpath)
 
