02d0e55837165f9c1d8c9824aa87d72e832d8be6
!git for-each-ref --format='%(refname:short)' `git symbolic-ref HEAD` <ben.dichter@gmail.com>
2020-01-22 19:57:57 -0500

    add test_io

---
+++ src/pynwb/tests/test_io.py
@@ -0,0 +1,20 @@
+from datetime import datetime
+from pynwb import NWBFile, NWBHDF5IO
+from ndx_point_cloud_table import PointCloudTable
+
+nwb = NWBFile('session_description', 'identifier', datetime.now().astimezone())
+
+point_cloud_table = PointCloudTable()
+
+point_cloud_table.add_row(point_cloud=[[1., 1., 1.], [2., 2., 2.]], timestamps=.4)
+
+behavior_mod = nwb.create_processing_module('behavior', 'desc')
+nwb.processing['behavior'].add(point_cloud_table)
+
+# Write nwb file
+with NWBHDF5IO('test_pointcloudtable.nwb', 'w') as io:
+    io.write(nwb)
+
+# Read nwb file and check its content
+with NWBHDF5IO('test_pointcloudtable.nwb', 'r', load_namespaces=True) as io:
+    nwb2 = io.read()
